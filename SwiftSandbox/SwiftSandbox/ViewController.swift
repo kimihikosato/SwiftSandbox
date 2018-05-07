b768565d6ca374f1132b4e428609eb27972bf6d6
kimihikosato <kimihiko.sato2@g.softbank.co.jp>
2018-05-07 13:31:06 +0900

    Initial commit

---
+++ SwiftSandbox/SwiftSandbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSandbox
+//
+//  Created by Kimihiko Satou on 2018/05/07.
+//  Copyright © 2018 Kimihiko Satou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
