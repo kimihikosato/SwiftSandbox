123160b2cc90476e0ac5fa2f28ce65b2f86243d1
kimihikosato <kimihiko.sato2@g.softbank.co.jp>
2018-05-07 13:33:29 +0900

    P.116

--- SwiftSandbox/SwiftSandbox/ViewController.swift
+++ SwiftSandbox/SwiftSandbox/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		//P.116
+		var num = 1
+		for _ in 0..<8 {
+			num *= 2
+		}
+		print( "num is " + String(num) )
+		
+		//P.116
+		//C++風
+		for i in 0..<8 {
+			print( "i is " + String(i) )			
+		}
+
 	}
 
 	override func didReceiveMemoryWarning() {
