dd86e394b6966dbba3313932a3ed4300cf913127
kimihikosato <kimihiko.sato2@g.softbank.co.jp>
2018-05-08 13:08:11 +0900

    P.131

--- SwiftSandbox/SwiftSandbox/ViewController.swift
+++ SwiftSandbox/SwiftSandbox/ViewController.swift
@@ -27,6 +27,42 @@ class ViewController: UIViewController {
 			print( "i is " + String(i) )			
 		}
 
+		print()
+		
+		//P.126
+		/**
+		var total: UInt32 = 0
+		func randomAndIncrement() {
+			let r = arc4random() % 10 + 1
+			print( "rand=\(r)" )
+			total += r
+		}
+		//func call 3 times
+		randomAndIncrement()
+		randomAndIncrement()
+		randomAndIncrement()
+		//
+		print( total )
+		**/
+		print()
+
+		//P.131
+		var total: UInt32 = 0
+		func generateRandomNumber() -> UInt32 {
+			return arc4random() % 10 + 1
+		}
+		func randomAndIncrement() {
+			let r = generateRandomNumber()
+			print( "rand=\(r)" )
+			total += r
+		}
+		//func call 3 times
+		randomAndIncrement()
+		randomAndIncrement()
+		randomAndIncrement()
+		//		
+		print( total )
+
 	}
 
 	override func didReceiveMemoryWarning() {
